64a44a9a33d7b68e4259f8977fd217d9cfa13061
Dan Fishgold <d.fishgold@gmail.com>
2017-07-21 18:22:36 +0300

    Added basis for audio ports and filled the update function

--- Main.elm
+++ Main.elm
@@ -1,8 +1,9 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import Html exposing (Html, program)
-import Html exposing (div, input, p, text)
+import Html exposing (div, input, p, h2, text)
 import Html.Events exposing (onInput)
+import Html.Attributes exposing (dir)
 import Keyboard exposing (KeyCode)
 
 
@@ -12,39 +13,121 @@ type alias TimeStamp =
 
 type alias Model =
     { url : String
+    , ready : Bool
     , timeStamp : TimeStamp
-    , currentNote : String
+    , currentNote : ( TimeStamp, String )
     , notes : List ( TimeStamp, String )
     }
 
 
 type Msg
     = EditUrl String
+    | IsReady Bool
     | EditCurrentNote String
+    | AddNote ( TimeStamp, String )
     | SetTimeStamp TimeStamp
     | KeyUp KeyCode
 
 
+port pauseUnpause : () -> Cmd msg
+
+
+port seek : Int -> Cmd msg
+
+
+port setUrl : String -> Cmd msg
+
+
+port timeStamp : (TimeStamp -> msg) -> Sub msg
+
+
+port isReady : (Bool -> msg) -> Sub msg
+
+
 init : ( Model, Cmd Msg )
 init =
-    ( { url = "", timeStamp = 0, currentNote = "", notes = [] }, Cmd.none )
+    ( { url = ""
+      , ready = False
+      , timeStamp = 0
+      , currentNote = ( 0, "" )
+      , notes = []
+      }
+    , Cmd.none
+    )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Keyboard.ups KeyUp
+    Sub.batch
+        [ Keyboard.ups KeyUp
+        , timeStamp SetTimeStamp
+        , isReady IsReady
+        ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        _ ->
+        EditUrl url ->
+            ( { model | url = url }, setUrl url )
+
+        IsReady ready ->
+            ( { model | ready = ready }, Cmd.none )
+
+        EditCurrentNote noteText ->
+            let
+                ( prevTimeStamp, prevText ) =
+                    model.currentNote
+
+                timeStamp =
+                    if String.isEmpty prevText then
+                        model.timeStamp
+                    else
+                        prevTimeStamp
+            in
+                ( { model | currentNote = ( timeStamp, noteText ) }, Cmd.none )
+
+        SetTimeStamp timeStamp ->
+            ( { model | timeStamp = timeStamp }, Cmd.none )
+
+        AddNote newNote ->
+            ( { model | notes = newNote :: model.notes }, Cmd.none )
+
+        KeyUp 13 ->
+            if not (String.isEmpty model.url) && not (String.isEmpty <| Tuple.second model.currentNote) then
+                update (AddNote model.currentNote) model
+            else
+                ( model, Cmd.none )
+
+        KeyUp 32 ->
+            ( model, pauseUnpause () )
+
+        KeyUp 37 ->
+            ( model, seek 5 )
+
+        KeyUp 39 ->
+            ( model, seek -5 )
+
+        KeyUp key ->
             ( model, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
-    text "hey"
+    div [ dir "rtl" ]
+        [ p [] [ text "url" ]
+        , input [ onInput EditUrl ] [ text model.url ]
+        , p [] [ text "new note" ]
+        , text <| toString <| Tuple.first model.currentNote
+        , input [ onInput EditCurrentNote ] [ text <| Tuple.second model.currentNote ]
+        , h2 [] [ text "notes" ]
+        , model.notes |> List.map noteEntry |> div []
+        ]
+
+
+noteEntry : ( TimeStamp, String ) -> Html msg
+noteEntry ( timeStamp, note ) =
+    p [] [ text <| toString timeStamp ++ " " ++ note ]
 
 
 main : Program Never Model Msg

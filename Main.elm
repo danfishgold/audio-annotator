54730e080a259b0537d55fb6821557604fa8811f
Dan Fishgold <d.fishgold@gmail.com>
2017-07-21 18:48:55 +0300

    It works!

--- Main.elm
+++ Main.elm
@@ -3,7 +3,7 @@ port module Main exposing (..)
 import Html exposing (Html, program)
 import Html exposing (div, input, p, h2, text)
 import Html.Events exposing (onInput)
-import Html.Attributes exposing (dir)
+import Html.Attributes exposing (dir, value)
 import Keyboard exposing (KeyCode)
 
 
@@ -11,20 +11,25 @@ type alias TimeStamp =
     Int
 
 
+type alias Note =
+    { timeStamp : TimeStamp, text : String }
+
+
 type alias Model =
     { url : String
     , ready : Bool
     , timeStamp : TimeStamp
-    , currentNote : ( TimeStamp, String )
-    , notes : List ( TimeStamp, String )
+    , currentNote : Note
+    , notes : List Note
     }
 
 
 type Msg
     = EditUrl String
     | IsReady Bool
-    | EditCurrentNote String
-    | AddNote ( TimeStamp, String )
+    | SetCurrentNoteText String
+    | SetCurrentNoteTime TimeStamp
+    | AddNote Note
     | SetTimeStamp TimeStamp
     | KeyUp KeyCode
 
@@ -49,7 +54,7 @@ init =
     ( { url = ""
       , ready = False
       , timeStamp = 0
-      , currentNote = ( 0, "" )
+      , currentNote = Note 0 ""
       , notes = []
       }
     , Cmd.none
@@ -74,39 +79,63 @@ update msg model =
         IsReady ready ->
             ( { model | ready = ready }, Cmd.none )
 
-        EditCurrentNote noteText ->
-            let
-                ( prevTimeStamp, prevText ) =
-                    model.currentNote
+        SetCurrentNoteText noteText ->
+            if model.currentNote.text == "" && noteText == " " then
+                ( model, Cmd.none )
+            else
+                let
+                    prevNote =
+                        model.currentNote
+                in
+                    ( { model | currentNote = { prevNote | text = noteText } }
+                    , Cmd.none
+                    )
 
-                timeStamp =
-                    if String.isEmpty prevText then
-                        model.timeStamp
-                    else
-                        prevTimeStamp
+        SetCurrentNoteTime timeStamp ->
+            let
+                prevNote =
+                    model.currentNote
             in
-                ( { model | currentNote = ( timeStamp, noteText ) }, Cmd.none )
+                ( { model | currentNote = { prevNote | timeStamp = timeStamp } }
+                , Cmd.none
+                )
 
         SetTimeStamp timeStamp ->
-            ( { model | timeStamp = timeStamp }, Cmd.none )
+            let
+                newModel =
+                    { model | timeStamp = timeStamp }
+            in
+                if model.currentNote.text == "" then
+                    update (SetCurrentNoteTime timeStamp) newModel
+                else
+                    ( newModel, Cmd.none )
 
         AddNote newNote ->
             ( { model | notes = newNote :: model.notes }, Cmd.none )
 
         KeyUp 13 ->
-            if not (String.isEmpty model.url) && not (String.isEmpty <| Tuple.second model.currentNote) then
-                update (AddNote model.currentNote) model
+            if not (String.isEmpty model.url) && not (String.isEmpty model.currentNote.text) then
+                update (AddNote model.currentNote) { model | currentNote = Note 0 "" }
             else
                 ( model, Cmd.none )
 
         KeyUp 32 ->
-            ( model, pauseUnpause () )
+            if String.isEmpty model.currentNote.text then
+                ( model, pauseUnpause () )
+            else
+                ( model, Cmd.none )
 
         KeyUp 37 ->
-            ( model, seek 5 )
+            if String.isEmpty model.currentNote.text then
+                ( model, seek 5 )
+            else
+                ( model, Cmd.none )
 
         KeyUp 39 ->
-            ( model, seek -5 )
+            if String.isEmpty model.currentNote.text then
+                ( model, seek -5 )
+            else
+                ( model, Cmd.none )
 
         KeyUp key ->
             ( model, Cmd.none )
@@ -116,18 +145,18 @@ view : Model -> Html Msg
 view model =
     div [ dir "rtl" ]
         [ p [] [ text "url" ]
-        , input [ onInput EditUrl ] [ text model.url ]
+        , input [ onInput EditUrl, value model.url ] []
         , p [] [ text "new note" ]
-        , text <| toString <| Tuple.first model.currentNote
-        , input [ onInput EditCurrentNote ] [ text <| Tuple.second model.currentNote ]
+        , text <| toString <| model.currentNote.timeStamp
+        , input [ onInput SetCurrentNoteText, value model.currentNote.text ] []
         , h2 [] [ text "notes" ]
         , model.notes |> List.map noteEntry |> div []
         ]
 
 
-noteEntry : ( TimeStamp, String ) -> Html msg
-noteEntry ( timeStamp, note ) =
-    p [] [ text <| toString timeStamp ++ " " ++ note ]
+noteEntry : Note -> Html msg
+noteEntry { timeStamp, text } =
+    p [] [ Html.text <| toString timeStamp ++ " " ++ text ]
 
 
 main : Program Never Model Msg

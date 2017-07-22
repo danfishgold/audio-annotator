4b42055c0ded863a11258c797dd8b529b42642cc
Dan Fishgold <d.fishgold@gmail.com>
2017-07-22 13:22:59 +0300

    Added an assets file

---
+++ Assets.elm
@@ -0,0 +1,5 @@
+module Assets exposing (..)
+
+-- This file contains SVGs from http://www.parakeet.co/primaries/
+-- I did not include them in the git repository
+-- because that's probably illegal.
